511d496229c14bfdd1a7d70e6f608132458c54c7
Павел <pashanoy@gmail.com>
2020-05-20 19:08:03 +0300

    Added books and authors (add in admin, show in index)

--- books/models.py
+++ books/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=500)
+    authors = models.ManyToManyField(Author, related_name="Books")
+    publisher = models.CharField(max_length=500)
+    isbn = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def index(request):
+    rand_books = Book.objects.all().order_by("?")[:5]
+    print(rand_books[0].authors.all())
+    return render(request, 'books/index.html', {'books': rand_books})
