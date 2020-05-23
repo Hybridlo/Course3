b8737389516db822e9ca5953cb9d29a7e4a2369f
Павел <pashanoy@gmail.com>
2020-05-23 23:51:54 +0300

    Unify index and search

--- books/views.py
+++ books/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Book
+from haystack.views import SearchView
+from haystack.forms import SearchForm
 
 def index(request):
+    if (request.GET.get("q", None) != None):
+        return SearchView(template="books/index.html")(request)
+
     rand_books = Book.objects.all().order_by("?")[:5]
     print(rand_books[0].authors.all())
-    return render(request, 'books/index.html', {'books': rand_books})
+    return render(request, 'books/index.html', {'books': rand_books, 'query': None, 'form': SearchForm()})
