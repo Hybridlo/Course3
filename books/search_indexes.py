8f4f217323699f4f70861ba108692ea2953e8b42
Павел <pashanoy@gmail.com>
2020-05-31 00:02:07 +0300

    Boosting

--- books/search_indexes.py
+++ books/search_indexes.py
@@ -4,6 +4,8 @@ from .models import Book
 
 class NoteIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    title = indexes.CharField(model_attr='title', boost=1.5)
+    tags = indexes.CharField(model_attr='tags', boost=1.25)
 
     def get_model(self):
         return Book
